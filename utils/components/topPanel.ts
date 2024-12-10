d63380caa6792f77942f290fd1fbffec6e7c3472
Yahor Shkliaryk <yshkliaryk@evolution.com>
2024-12-10 20:27:59 +0100

    Initial empty commit for main

--- utils/components/topPanel.ts
+++
@@ -1,9 +0,0 @@
-import { Locator, Page } from "@playwright/test";
-
-export class TopPanel {
-    public constructor(private page: Page) { }
-
-    public getTopPanel(): Locator {
-        return this.page.locator("id=top");
-    }
-}
