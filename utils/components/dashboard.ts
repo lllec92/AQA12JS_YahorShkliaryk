3a836001b05eb266fe1e645c6eaa4badf41235b5
Yahor Shkliaryk <yshkliaryk@evolution.com>
2024-12-10 20:27:59 +0100

    Initial empty commit for main

--- utils/components/dashboard.ts
+++
@@ -1,9 +0,0 @@
-import { Locator, Page } from "@playwright/test";
-
-export class Dashboard {
-    public constructor(private page: Page) { }
-
-    public getAddProjectButton(): Locator {
-        return this.page.getByTestId("sidebarProjectsAddButton");
-    }
-}
